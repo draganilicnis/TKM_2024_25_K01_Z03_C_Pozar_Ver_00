dc206681faad227ebc4d37f1b5eec737d91df54f
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2024-11-25 21:23:24 +0100

    Create TKM_2024_25_K01_Z03_C_Pozar.cs

---
+++ TKM_2024_25_K01_Z03_C_Pozar.cs
@@ -0,0 +1,13 @@
+// https://arena.petlja.org/sr-Latn-RS/competition/2024-2025-kv1-testiranje#tab_137833
+// https://algora.petlja.org/t/2024-2025/9035/3
+// 
+
+using System;
+
+class TKM_2024_25_K01_Z03_C_Pozar_Ver_00
+{
+    static void Main()
+    {
+        Console.WriteLine("H");
+    }
+}
